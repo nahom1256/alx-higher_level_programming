626118cc9eb11918fb22e8bc2e2382db3211f61e
nahom1256 <theweeknd1256@gmail.com>
2023-12-19 08:55:33 -0800

    f

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square """
+
+
+class Square:
+    """Defines an empty square."""
+
+    def __init__(self, size):
+        """Constructor.
+        Args:
+            size: side of square 
+        """
+        self.__size = size
